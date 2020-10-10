ba4620f710cf0da8141e210cc9598177a255cda8
Sorin Sbarnea <ssbarnea@users.noreply.github.com>
2020-10-10 13:03:23 +0100

    Workaround for pip failure to perform editable install (#55)
    Related: https://github.com/pypa/pip/issues/7953

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
 # !/usr/bin/env python
+import site
+import sys
+
 import setuptools
+
+# See https://github.com/pypa/pip/issues/7953
+site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
 
 
 if __name__ == "__main__":
-    setuptools.setup(use_scm_version=True)
+    setuptools.setup(
+        use_scm_version={"local_scheme": "no-local-version"},
+        setup_requires=["setuptools_scm[toml]>=3.5.0"],
+    )
